aed43b2463681cfb45c1ec8aaae0f84811f82d33
Tony Dew <git@tonydew.com>
2012-10-12 00:16:12 -0700

    Successful submission view

---
+++ application/views/woot_v.php
@@ -0,0 +1,5 @@
+<?php
+
+	echo '<p>Form data was insterted. Now go check the database to see if it was correctly prepared before instertion!<p>';
+
+	echo '<br />[' . anchor('welcome/standard', 'Standard Test') . '] [' . anchor('welcome/mymodel', 'MY_Model Test') .']';
